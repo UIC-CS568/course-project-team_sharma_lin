63a79e85c2ecfd737aa94bc5118a2c52f15e4bad
xlin29 <xlin48@uic.edu>
2020-10-09 21:46:59 -0500

    crawler

--- crawl.py
+++ crawl.py
@@ -10,19 +10,9 @@ import time
 
 
 def run_onedomain(domain_url):
-    #file = "fingerprinting_domains.json"
     binary_path = '/home/xlin/VisibleV8/visible_chromium/src/out/Builder/chrome'
     executable_path = '/home/xlin/Tools/chromedriver_75/chromedriver'
     timestr = str(time.strftime("%Y%m%d_%H%M%S"))
-    #urls = []
-    #with open(file, 'r') as f:
-    #    s = f.read()
-    #    data = json.loads(s)
-    #    for line in data:
-    #        url_ls = data[line]
-    #        for each in url_ls:
-    #            urls.append(each['top_url'])
-    #print(len(urls))
 
     domain_dir = domain_url.replace('/', '_')
     domain_dir = domain_dir.replace(':', '_')
@@ -36,8 +26,7 @@ def run_onedomain(domain_url):
     options.headless = False
     options.binary_location = binary_path
     options.add_argument('--no-sandbox')
-    
-    #for each_url in urls:
+
     try:
         #fld = get_tld(each_url, as_object=True).fld
         fld = get_tld(domain_url, as_object=True).fld
@@ -45,7 +34,7 @@ def run_onedomain(domain_url):
         if 'http' not in domain_url:
             domain_url = 'http://'+domain_url
         driver.get(domain_url)
-        time.sleep(10)
+        time.sleep(15)
         print(fld)
     except Exception as e:
         print(str(e))
